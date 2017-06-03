c64779500e42e5a48f90d0c5ed34bd151c557c7c
Justin Warger <justin@mymac.ca>
2017-06-03 10:08:47 -0700

    Add files via upload
    Overhaul of new site design.

--- index.php
+++ index.php
@@ -30,7 +30,6 @@
           <a class="nav-link bg-info text-white" href="#services">What I Offer</a>
           <a class="nav-link bg-info text-white" href="#languages">Languages</a>
           <a class="nav-link bg-info text-white" href="#treehouse">Treehouse</a>
-          <a class="nav-link bg-info text-white" href="#process">The Process</a>
           <a class="nav-link bg-info text-white rounded-right" href="#contact">Contact</a>
         </nav>
     </div>
@@ -38,12 +37,12 @@
     <!-- gettoknow -->
     <div class="container" id="gettoKnow">
       <div class="row">
-        <div class="col-xl-4 col-lg-12 text-center mb-3">
+        <div class="col-xl-4 col-lg-12 text-center mb-3 py-5">
           <img src="/images/profile.jpg" class="rounded-circle img-fluid" alt="Picture of me!">
         </div>
         <div class="text-center col-xl-8 col-lg-12 p-5">
           <h3 class="display-4 text-info" id="gettoknow">Get to know me</h3>
-          <p>I am intrigued by the web. It all started when I was young. I would watch people "hacking" in movies and tv. I was exhillerated! It took me several years to realize that I was capable of designing and building things on the web. That's when passion took over. I wrote my first "Hello, World" heading on a webpage and I was hooked. Every opportunity I get to learn and work with the web I am grateful for. It's exciting watching technology evolve and grow. Web design is that perfect combination of logic and beauty. It is much more than just a page with images and text. Take the opportunity to let me show you what the web is capable of. Let's make something great!   </p>
+          <p>I love programming! I have spent a large chunk of my life building things with my hands. I was a plumber, even went to school for it! I have been a certified crane operator working in the oilfield. And spent some time in mechanics through high school. What I learned from all this is, it does not matter what you are using. Building things is a skill that is highly challenging and equally rewarding. When I was very young I used to see people "hacking" in the movies. I was so curious and excited by watching people break into "mainframes". I didn't realize what they were doing was in fact real. I grew up believing that it was impossible to explore and build things using a computer. I spent many childhood years opening up PC's and learning about what was inside them for hardware. I was fortunate to have people in my life who knew computers and were willing to show me what they knew. As I got older I started to realize what was actually inside the computer in terms of software and how it worked. That is when I discovered coding. That was the day my passion for computers truly developed. I have spent every day since then learning about everything code. It has been a childhood dream come true. </p>
         </div>
       </div>
     </div>
@@ -54,25 +53,24 @@
         <h1 class="text-center mb-5">What I offer</h1>
         <div class="row text-center">
           <div class="col-lg-4">
-            <h3 class="mb-3 text-info">Front End Development</h3>
-            <p class="p-2">Design and functionality are the keys to success. By using the right layout your website will be an experience your clients or customers will love. My goal is to achieve the maximum potential for your digital storefront.</p>
+            <h3 class="mb-3 text-info">Communication</h3>
+            <p class="p-2">Communication is one of the biggest factors to operating at a high level. Being able to understand and relay messages in a clear and concise manner is a skill many lack. I take pride in my skill to communicate. It's constantly under development and love taking advatage of the many platforms out there used to communicate.</p>
           </div>
           <div class="col-lg-4">
-            <h3 class="mb-3 text-info">Maintenance</h3>
-            <p class="p-2">Keeping your site modern and up to date is a never ending process. I don't believe in building something and then abandoning it. Making it easy for you to update and work with your site is important. Making myself available to you is my top priority.</p>
+            <h3 class="mb-3 text-info">Reliability</h3>
+            <p class="p-2">Nothing is more comforting than a reliable person. Like a good car you know it's always going to be there for you. This is one quality I love shining a spotlight on. It really highlights the kind of personality I have. I believe being a reliable person is also being a respectful one. What comes around goes around. And respect goes a long way.</p>
           </div>
           <div class="col-lg-4">
-            <h3 class="mb-3 text-info">Back End Development</h3>
-            <p class="p-2">Behind the webpage is where the real magic happens. The initial design is very important. But it is nothing without back end functionality. It's like a beautiful car without an engine. I can give your website the engine it needs to run.</p>
+            <h3 class="mb-3 text-info">Independence</h3>
+            <p class="p-2">I am self motivated. I taught myself to code! I am constantly pushing myself to do better. There is no greater competition then the one between you and yourself. Independence is a great quality to have in a team setting. I am not afraid to voice my opinion and work as a group towards a common goal.</p>
           </div>
         </div>
       </div>
     </div>
     <!-- /services -->
     <!-- languages -->
-    <!-- Add links to websites on images -->
     <div class="container text-center my-5" id="languages">
-      <h1 class="m-5">Languages I work with</h1>
+      <h1 class="m-5">Languages I know</h1>
       <div class="row mt-5">
         <div class="col-lg-3 mb-5">
           <img src="/images/html.png" class="img-fluid" id="html" alt="Language Logo">
@@ -88,10 +86,11 @@
         </div>
       </div>
     </div>
+    <?php include "inc/python.php" ?>
     <!-- /languages -->
     <!-- treehouse -->
     <!-- link treehouse pic to site -->
-    <div class="container-fluid" id="treehouse">
+    <div class="container-fluid mt-3" id="treehouse">
       <div class="jumbotron text-center">
         <a href="https://teamtreehouse.com/justinwarger" target="blank">
           <img src="/images/treehouse.png" class="img-fluid mt-5">
@@ -99,74 +98,7 @@
         <h3 class="my-5">Check out my profile to get a feel for the courses I have taken and skills I have.</h3>
       </div>
     </div>
-    <!-- treehouse -->
-    <!-- process -->
-    <div class="container" id="process">
-      <h1 class="display-3 text-muted text-center my-5" id="theprocess">The Process</h1>
-      <div class="row my-5 pb-5" id="info">
-        <div class="col-lg-4 text-right col-xs-4 text-center mb-5">
-          <img src="/images/mail.png" height="150px" width="150px">
-        </div>
-        <div class="col-lg-8 text-left mt-2">
-          <h1 class="text-muted">First Meeting</h1>
-          <p>This is our first time talking to each other! It can be through any form that works for you. My main goal is to figure out what the goal of your website is. To showcase your work, or make it easy for people to see what you are all about.</p>
-        </div>
-      </div>
-      <div class="row my-5 pb-5" id="info">
-        <div class="col-lg-4 text-right col-xs-4 text-center mb-5">
-          <img src="/images/layout.png" height="150px" width="150px" alt="Small Round Image">
-        </div>
-        <div class="col-lg-8 text-left mt-2">
-          <h1 class="text-muted">Site Structure & Layout</h1>
-          <p>After I have an idea of what goal you need your website to accomplish. I can start formulating a layout and structure that will function efficiently for your users. This is where you will start to see where information will be on the page.</p>
-        </div>
-      </div>
-      <div class="row my-5 pb-5" id="info">
-        <div class="col-lg-4 text-right col-xs-4 text-center mb-5">
-          <img src="/images/phone.png" height="150px" width="150px" alt="Small Round Image">
-        </div>
-        <div class="col-lg-8 text-left mt-2">
-          <h1 class="text-muted">Working Prototype</h1>
-          <p>Now that I have a plan in place and structure established I will start developing a rough design layout. This is where you will be able to see how the content moves and looks on all devices.</p>
-        </div>
-      </div>
-      <div class="row my-5 pb-5" id="info">
-        <div class="col-lg-4 text-right col-xs-4 text-center mb-5">
-          <img src="/images/details.png" height="150px" width="150px" alt="Small Round Image">
-        </div>
-        <div class="col-lg-8 text-left mt-2">
-          <h1 class="text-muted">Details</h1>
-          <p>The information is on the page and the color scheme and images are working together. Now I will add all the details. These include links, hover states, navigation, and many other things required for an amazing website. </p>
-        </div>
-      </div>
-      <div class="row my-5 pb-5" id="info">
-        <div class="col-lg-4 text-right col-xs-4 text-center mb-5">
-          <img src="/images/lock.png" height="150px" width="150px" alt="Small Round Image">
-        </div>
-        <div class="col-lg-8 text-left mt-2">
-          <h1 class="text-muted">You're in Control</h1>
-          <p>I hand over the keys to you. I use Wordpress to give you the control panel to add and change anything you want on your site. You do not have to know how to code!</p>
-        </div>
-      </div>
-      <div class="row my-5 pb-5">
-        <div class="col-lg-4 text-right col-xs-4 text-center mb-5">
-          <img src="/images/globe.png" height="150px" width="150px" alt="Small Round Image">
-        </div>
-        <div class="col-lg-8 text-left mt-2">
-          <h1 class="text-muted">Deployment</h1>
-          <p>Your site is officially launched! The process does not end there. I will always be available for support and design consulting. Time to celebrate and let the world see your website!</p>
-        </div>
-      </div>
-    </div>
-    <!-- /process -->
-    <!-- custom -->
-    <div class="container-fluid my-5">
-      <div class="jumbotron text-left text-muted text-center px-5">
-        <h1 class="mb-5">A Custom Responsive Design</h1>
-        <h6>I specialize in creating an experience for your users that is tailored to your needs. A completely custom design that looks fantastic on all devices. Together we can maximize the potential of your online presence. The value of a website can be easily overlooked. Don't make the mistake of undervaluing the potential of a quality web page.</h6>
-      </div>
-    </div>
-    <!-- /custom -->
+    <!-- /treehouse -->
     <!-- contact -->
     <?php include "inc/contact.php"; ?>
     <!-- /contact -->
